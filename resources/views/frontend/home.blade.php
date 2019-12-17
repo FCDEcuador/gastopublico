a1f4f866dde02fc77eb502c41c89ebec072d91a3
Pablo Reyes <yellowgamepjrr@gmail.com>
2019-12-16 22:10:12 -0500

    fix home slide

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -74,7 +74,7 @@
 									</div>
 									<div class="col-sm-7">
 										<h6 class="mb-0 ml-sm-3 mb-0 mt-3 mt-sm-0">
-											<a href="{!! route('content-article', [$oSecondaryArticle->contentCategory->slug, $oSecondaryArticle->slug]) !!}" class="text-muted" class="public-sidebar">{{ $oSecondaryArticle->summary }}</a>
+											<a href="{!! route('content-article', [$oSecondaryArticle->contentCategory->slug, $oSecondaryArticle->slug]) !!}" class="text-muted public-sidebar" >{{ $oSecondaryArticle->summary }}</a>
 										</h6>
 										<div class="text-right text-muted fz12 mt-1">
 										      {!! TimeFormat::dateShortFormat($oSecondaryArticle->publication_date ? $oSecondaryArticle->publication_date : $oSecondaryArticle->created_at) !!}
