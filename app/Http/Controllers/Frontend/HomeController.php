c7b24420a84f229ec1d5b499ee845ebce517496d
Raul Chauvin <raul.chauvin@telefonica.com>
2020-06-16 19:55:10 -0500

    Correccion en articulos destacados

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -170,7 +170,7 @@ class HomeController extends Controller
             
             // Datos para el contenido de la pagina
             'aContentCategories' => $aContentCategories,
-            'mainArticles' => ContentArticle::available()->orderBy('publication_date', 'desc')->skip(0)->take(3)->get(),
+            'mainArticles' => ContentArticle::available()->outstandings()->orderBy('publication_date', 'desc')->skip(0)->take(3)->get(),
             'secondaryArticles' => is_object($oPublicacionesCategory) ? $oPublicacionesCategory->contentArticles()->available()->orderBy('publication_date', 'desc')->take(4)->get() : ContentArticle::available()->orderBy('publication_date', 'desc')->skip(3)->take(4)->get(),
             'listArticles' => is_object($oNoticiasCategory) ? $oNoticiasCategory->contentArticles()->available()->orderBy('publication_date', 'desc')->skip(0)->take(3)->get() : $oNoticiasCategory->contentArticles()->available()->orderBy('publication_date', 'desc')->skip(3)->take(3)->get(),
             'aIndicators' => Indicator::where('unity', 'USD')->orderBy('created_at', 'desc')->skip(0)->take(4)->get(),
